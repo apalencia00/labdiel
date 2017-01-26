37f77a161f8211a314a88eb2bdfe0d41f9498ce2
Andres Palencia <palencia89@gmail.com>
2017-01-26 10:58:19 -0500

    Comiteando server Amazon V7.38

--- frontend/Ensayo_Equipo.php
+++ frontend/Ensayo_Equipo.php
@@ -56,9 +56,9 @@ if( $_SESSION['admon_mod'] != 0 || $_SESSION['admon_mod'] != "" || $_SESSION['ad
 
             <div class="form-group">
               <label for="tipodoc" class="control-label col-sm-2">Cliente</label>
-              <div class="col-sm-4 col-xs-7">
+              <div class="col-sm-6 col-xs-5">
 
-               <select id="listCliente" class="form-control col-sm-4">
+               <select id="listCliente" class="form-control">
                 <option>Seleccione </option>
               </select>
                <span class="help-block"></span>
