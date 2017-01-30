7a5e595d6a8fb50b062d50a44907e4db473f867e
Andres Palencia <palencia89@gmail.com>
2017-01-29 19:26:15 -0500

    Comieando myServer Fix Reportes Amazon 4.3

--- frontend/Registro_Equipos.php
+++ frontend/Registro_Equipos.php
@@ -83,7 +83,7 @@ if( $_SESSION['admon_mod'] != 0 || $_SESSION['admon_mod'] != "" || $_SESSION['ad
           <label for="numdoc" class="control-label col-sm-4">Codigo Equipo</label>
             <div class="col-sm-8">
               <input type="text"  id="cod_equipo" placeholder="Codigo Producto" class="form-control col-lg-100" autofocus>
-              
+              <small id="emailHelp" class="form-text text-muted">Ej. G000-1A</small>
             </div>
 
             
